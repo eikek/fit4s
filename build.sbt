227f4f291a263a05dabd54fbd34f89b23e9e0e9e
Eike Kettner <eike.kettner@posteo.de>
2019-07-07 13:19:05 +0200

    Update dependencies

--- build.sbt
+++ build.sbt
@@ -1,26 +1,35 @@
 import libs._
 
+val scalacOpts: Seq[String] = Seq(
+  "-encoding", "UTF-8",
+  "-Xfatal-warnings",
+  "-deprecation",
+  "-feature",
+  "-unchecked",
+  "-language:higherKinds",
+  "-Xlint",
+  "-Yno-adapted-args",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Ywarn-unused-import"
+)
+
 lazy val sharedSettings = Seq(
   scalaVersion := `scala-version`,
-  scalacOptions ++= Seq(
-    "-encoding", "UTF-8",
-    "-Xfatal-warnings", // fail when there are warnings
-    "-deprecation",
-    "-feature",
-    "-unchecked",
-    "-language:higherKinds",
-    "-Xlint",
-    "-Yno-adapted-args",
-    "-Ywarn-dead-code",
-    "-Ywarn-numeric-widen",
-    "-Ywarn-unused-import"
-  ),
+  scalacOptions := {
+    if (scalaBinaryVersion.value.startsWith("2.13")) {
+      scalacOpts.filter(o => o != "-Yno-adapted-args" && o != "-Ywarn-unused-import")
+    } else {
+      scalacOpts
+    }
+  },
   scalacOptions in (Compile, console) ~= (_ filterNot (Set("-Xfatal-warnings", "-Ywarn-unused-import").contains)),
-  scalacOptions in (Test) := (scalacOptions in (Compile, console)).value
+  scalacOptions in (Test, console) := (scalacOptions in (Compile, console)).value,
+  crossScalaVersions := Seq("2.12.8", `scala-version`)
 )
 
 lazy val coreDeps = Seq(log4s, `scodec-core`)
-lazy val testDeps = Seq(scalatest, scalacheck, `logback-classic`, `fs2-core`, `fs2-io`).map(_ % "test")
+lazy val testDeps = Seq(minitest, scalacheck, `logback-classic`, `fs2-core`, `fs2-io`).map(_ % "test")
 
 lazy val core = project.in(file("modules/core")).
   settings(sharedSettings).
--- project/libs.scala
+++ project/libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object libs {
 
-  val `scala-version` = "2.12.4"
+  val `scala-version` = "2.13.0"
 
   // https://github.com/melrief/pureconfig
   // MPL 2.0
@@ -14,35 +14,27 @@ object libs {
 
   // https://github.com/functional-streams-for-scala/fs2
   // MIT
-  val `fs2-core` = "co.fs2" %% "fs2-core" % "0.9.6"
-  val `fs2-io` = "co.fs2" %% "fs2-io" % "0.9.6"
+  val `fs2-core` = "co.fs2" %% "fs2-core" % "1.1.0-M1"
+  val `fs2-io` = "co.fs2" %% "fs2-io" % "1.1.0-M1"
 
-  // https://github.com/scalatest/scalatest
-  // ASL 2.0
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.4"
+  // https://github.com/monix/minitest
+  // Apache 2.0
+  val minitest = "io.monix" %% "minitest" % "2.5.0"
+  val `minitest-laws` = "io.monix" %% "minitest-laws" % "2.5.0"
 
   // https://github.com/rickynils/scalacheck
   // unmodified 3-clause BSD
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.5"
+  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
 
   // https://github.com/scodec/scodec
   // 3-clause BSD
-  val `scodec-core` = "org.scodec" %% "scodec-core" % "1.10.3"
+  val `scodec-core` = "org.scodec" %% "scodec-core" % "1.11.4"
 
   // https://github.com/Log4s/log4s
   // ASL 2.0
-  val log4s = "org.log4s" %% "log4s" % "1.4.0"
+  val log4s = "org.log4s" %% "log4s" % "1.8.2"
 
   // http://logback.qos.ch/
   // EPL1.0 or LGPL 2.1
   val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.2.3"
-
-  // https://java.net/projects/javamail/pages/Home
-  // CDDL 1.0, GPL 2.0
-  val `javax-mail-api` = "javax.mail" % "javax.mail-api" % "1.5.6"
-  val `javax-mail` = "com.sun.mail" % "javax.mail" % "1.5.6"
-
-  // http://dnsjava.org/
-  // BSD
-  val dnsjava = "dnsjava" % "dnsjava" % "2.1.8" intransitive()
 }
