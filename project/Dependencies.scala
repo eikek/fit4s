878523448ccf780d926170986085f72cfa3e66f7
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-08-31 06:17:25 +0000

    Update flyway-core to 9.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val doobie = "1.0.0-RC4"
     val http4s = "0.23.23"
     val http4sDom = "0.2.9"
-    val flyway = "9.21.2"
+    val flyway = "9.22.0"
     val h2 = "2.2.220"
     val munit = "0.7.29"
     val munitCatsEffect = "1.0.7"
