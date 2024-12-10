b3b80d7b2bae6f9a096d9d0872fe5083f4c56544
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-12-10 10:06:24 +0000

    Update borer-compat-cats, ... to 1.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val scala2 = "2.13.10"
     val scala3 = "3.5.2"
 
-    val borer = "1.14.1"
+    val borer = "1.15.0"
     val catsEffect = "3.5.5"
     val catsParse = "1.0.0"
     val calico = "0.2.2"
