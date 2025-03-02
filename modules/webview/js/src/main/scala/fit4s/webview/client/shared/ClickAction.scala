8636397246591648245b99be319726a712cd69f2
eikek <eikek@posteo.de>
2025-03-02 14:01:39 +0100

    Update scala to 3.6.3

--- modules/webview/js/src/main/scala/fit4s/webview/client/shared/ClickAction.scala
+++ modules/webview/js/src/main/scala/fit4s/webview/client/shared/ClickAction.scala
@@ -1,6 +1,5 @@
 package fit4s.webview.client.shared
 
-import cats.FlatMap
 import cats.effect.Concurrent
 import cats.syntax.all.*
 import fs2.dom.MouseEvent
@@ -8,5 +7,5 @@ import fs2.{Pipe, Stream}
 
 object ClickAction:
 
-  def apply[F[_]: FlatMap: Concurrent](action: F[Unit]): Pipe[F, MouseEvent[F], Nothing] =
+  def apply[F[_]: Concurrent](action: F[Unit]): Pipe[F, MouseEvent[F], Nothing] =
     _.switchMap(ev => Stream.exec(action *> ev.preventDefault))
