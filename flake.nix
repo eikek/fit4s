eaf7271a11697571dfbb796fbae304caf68920c0
eikek <701128+eikek@users.noreply.github.com>
2025-05-07 23:59:38 +0200

    Fix tailwindcss styles (#446)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "fit4s flake";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     devshell-tools.url = "github:eikek/devshell-tools";
     sbt.url = "github:zaninime/sbt-derivation";
     sbt.inputs.nixpkgs.follows = "nixpkgs";
@@ -59,7 +59,7 @@
         ];
       };
       ciPkgs = with pkgs; [
-        sbt17 nodejs
+        sbt21 nodejs
       ];
     in {
       ci = pkgs.mkShellNoCC {
@@ -69,6 +69,7 @@
       default = pkgs.mkShellNoCC {
         buildInputs = ciPkgs ++ [
           pkgs.scala-cli
+          pkgs.tailwindcss
         ];
         nativeBuildInputs = [
         ];
